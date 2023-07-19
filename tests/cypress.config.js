01e6e4085dbfeb17081932cbfd510b5c598dd4cf
Antoine Aubry <antoine@aaubry.net>
2023-07-19 15:55:21 +0100

    Update tests URL

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://glutenfreemap.github.io"
+    baseUrl: "https://glutenfreemap.org"
   },
 });
