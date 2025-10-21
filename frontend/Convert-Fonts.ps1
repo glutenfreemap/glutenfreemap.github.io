66ad37dc39c9c017d41a9fa4ca06fc51f854f92a
Antoine Aubry <antoine@aaubry.net>
2025-10-21 18:24:11 +0100

    Improve font handling and add some icons

--- frontend/Convert-Fonts.ps1
+++ frontend/Convert-Fonts.ps1
@@ -1,5 +1,15 @@
 # Use https://www.glyphrstudio.com/app/ to edit the .gs2 project
 "ttf","woff","woff2" | % {
-  #& 'C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe' -lang=ff -c 'Open($1); Generate($2);' .\MaterialIconsSlim-Regular.otf ".\public\fonts\MaterialIconsSlim-Regular.$_"
-  fontforge -lang=ff -c 'Open($1); Generate($2);' MaterialIconsSlim-Regular.otf "./public/fonts/MaterialIconsSlim-Regular.$_"
+  fontforge -lang=ff -c 'Open($1); Generate($2);' "$PSScriptRoot/MaterialIconsSlim-Regular.otf" "$PSScriptRoot/public/fonts/MaterialIconsSlim-Regular.$_"
 }
+
+$hash = (Get-FileHash -Algorithm MD5 "$PSScriptRoot/MaterialIconsSlim-Regular.otf").hash
+
+"@font-face {
+  font-family: 'Material Icons Slim';
+  font-style: normal;
+  font-weight: 400;
+  src: url(/fonts/MaterialIconsSlim-Regular.woff2?v=$hash) format('woff2'),
+       url(/fonts/MaterialIconsSlim-Regular.ttf?v=$hash) format('truetype'),
+       url(/fonts/MaterialIconsSlim-Regular.woff?v=$hash) format('woff');
+}" | Set-Content "$PSScriptRoot/src/material-icons.scss"
